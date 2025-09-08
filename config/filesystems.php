d8b6bcffd9049976a5c30def676dffe449a328f7
XLQ <1393966473@qq.com>
2025-09-08 13:09:14 +0800

    基本結構調整

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,6 +60,34 @@ return [
             'report' => false,
         ],
 
+        'gcs_public' => [
+            'driver' => 'gcs',
+            'key_file_path' => env('GOOGLE_CLOUD_KEY_FILE', storage_path() . '/app/gcs_key.json'), // optional: /path/to/service-account.json
+            'key_file' => [], // optional: Array of data that substitutes the .json file (see below)
+            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'new-clock-in'), // optional: is included in key file
+            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'crepowermay-shared-file'),
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', 'chenfong'), // optional: /default/path/to/apply/in/bucket
+            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
+            'api_endpoint' => env('GOOGLE_CLOUD_STORAGE_API_ENDPOINT', null), // set storageClient apiEndpoint
+            'visibility' => 'public', // optional: public|private
+            'visibility_handler' => null, // optional: set to \League\Flysystem\GoogleCloudStorage\UniformBucketLevelAccessVisibility::class to enable uniform bucket level access
+            'metadata' => ['cacheControl' => 'public,max-age=86400'], // optional: default metadata
+        ],
+
+        'gcs_private' => [
+            'driver' => 'gcs',
+            'key_file_path' => env('GOOGLE_CLOUD_KEY_FILE', storage_path() . '/app/gcs_key.json'), // optional: /path/to/service-account.json
+            'key_file' => [], // optional: Array of data that substitutes the .json file (see below)
+            'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'new-clock-in'), // optional: is included in key file
+            'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'crepowermay-shared-file'),
+            'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', 'chenfong'), // optional: /default/path/to/apply/in/bucket
+            'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
+            'api_endpoint' => env('GOOGLE_CLOUD_STORAGE_API_ENDPOINT', null), // set storageClient apiEndpoint
+            'visibility' => 'private', // optional: public|private
+            'visibility_handler' => null, // optional: set to \League\Flysystem\GoogleCloudStorage\UniformBucketLevelAccessVisibility::class to enable uniform bucket level access
+            'metadata' => ['cacheControl' => 'public,max-age=86400'], // optional: default metadata
+        ],
+
     ],
 
     /*
